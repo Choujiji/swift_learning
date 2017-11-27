bc1c3fa08978f0aa205b3df8893e729abb291e64
zhaoji_choujiji@163.com <zhaoji_choujiji@163.com>
2017-11-27 17:02:24 +0800

    lecture_1代码提交

---
+++ Concentration/Concentration/ViewController.swift
@@ -0,0 +1,48 @@
+//
+//  ViewController.swift
+//  Concentration
+//
+//  Created by mac on 2017/11/27.
+//  Copyright © 2017年 jiji. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    var flipCount = 0 {
+        didSet {
+            flipCountLabel.text = "Flips: \(flipCount)"
+        }
+    }
+
+    @IBOutlet weak var flipCountLabel: UILabel!
+    
+    @IBOutlet var cardButtons: [UIButton]!
+    
+    var emojiChoices = ["🎃", "👻", "🎃", "👻"]
+    
+    @IBAction func touchCard(_ sender: UIButton) {
+        flipCount += 1
+        if let cardNumber = cardButtons.index(of: sender) {
+            print("cardNumber = \(cardNumber)")
+            flipCard(withEmoji: emojiChoices[cardNumber], on: sender)
+        } else {
+            print("not in cardButtons...")
+        }
+    }
+    
+    func flipCard(withEmoji emoji: String, on button: UIButton) {
+        print("flipCard(withEmoji: \(emoji))")
+        if button.currentTitle == emoji {
+            button.setTitle("", for: .normal)
+            button.backgroundColor = #colorLiteral(red: 0.99483639, green: 0.5957265496, blue: 0.1807449758, alpha: 1)
+        } else {
+            button.setTitle(emoji, for: .normal)
+            button.backgroundColor = #colorLiteral(red: 0.9999960065, green: 1, blue: 1, alpha: 1)
+        }
+    }
+    
+
+}
+
